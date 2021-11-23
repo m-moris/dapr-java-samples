ce3934ec11a3410adf5e4c5a600f3c1004deb9ea
MORISHIMA Masahito <morishima@pnop.co.jp>
2021-11-23 11:37:28 +0900

    change component files

--- run2.sh
+++ run2.sh
@@ -1,3 +1,3 @@
 dapr run --app-id backend --app-port 8888 --dapr-http-port 3501 \
-  --components-path ./backend/components \
+  --components-path ./backend/components/dev \
   -- java -jar backend/target/backend-0.0.1-SNAPSHOT.jar com.example.dapr.backend.BackEndApplication --server.port=8888
